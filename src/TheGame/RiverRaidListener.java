5ba1fbde310411fc5d9c17c8dcdd40799b6028d8
Fares Magd <faresmagd504@gmail.com>
2023-12-12 01:42:41 +0200

    fixing a bug

--- src/TheGame/RiverRaidListener.java
+++ src/TheGame/RiverRaidListener.java
@@ -3,6 +3,7 @@ package TheGame;
 import javax.media.opengl.GLEventListener;
 
 public abstract class RiverRaidListener extends KeyHandling implements GLEventListener {
+
     protected String assetsFolderName = "Assets";
 
 }
